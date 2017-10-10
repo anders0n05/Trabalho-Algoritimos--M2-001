64e1c00c6def7bc10908994f9160286cd71f2282
Anderson Silva <anderson.silva@edu.univali.br>
2017-10-10 01:03:52 -0300

    quick_sort implementado

--- Trabalho-m2-01/Trabalho-m2-01/implementação.h
+++ Trabalho-m2-01/Trabalho-m2-01/implementação.h
@@ -1,16 +1,48 @@
 #ifndef IMPLEMENTACAO_H
 #define IMPLEMENTACAO_H
 
+
+
+template<typename C>
+C dividir_vetor(C vetor[],int inicio, int fim) {
+	int antes_pivo, depois_pivo, aux;
+	antes_pivo = inicio;
+	depois_pivo =fim;
+	pivo = vetor[inicio];
+	while (antes_pivo < depois_pivo) {
+		while (vetor[antes_pivo] <= pivo) {//procurar valores menores que o pivo;
+			antes_pivo++;
+		 }
+		while (vetor[depois_pivo] > pivo) {//procurar valores maiores que o pivo;
+			depois_pivo--;
+		}
+
+		if (antes_pivo < depois_pivo) {
+
+			aux = vetor[depois_pivo];
+			vetor[antes_pivo] = vetor[depois_pivo];
+			vetor[depois_pivo] = aux;
+		}
+	}
+	vetor[inicio] = vetor[depois_pivo];
+	vetor[depois_pivo] = pivo;//ver se precisa disso pois la encima o pivo=vetor[inicio
+	return depois_pivo;
+}
+
+
+
+
 template<typename A>
-A quick_sort(A vetor[]) {
+A quick_sort(A vetor[],int inicio,int fim) {
 
+	if (fim > inicio){
+		pivo = dividir_vetor(vetor,inicio,fim);
+	quick_sort(vetor, inicio, pivo - 1); //ordenar elementos antes do pivo
+	quick_sort(vetor, pivo + 1, fim);//ordenar elementos depois do pivo
+	}
 
 }
 
-template<typename B>
-B insertion_sort(B vetor[]) {
 
-
-}
 
 #endif
--- Trabalho-m2-01/Trabalho-m2-01/testes.h
+++ Trabalho-m2-01/Trabalho-m2-01/testes.h
@@ -1,4 +1,7 @@
 
+#ifndef TESTES_H
+#define TESTES_H
+
 #include<iostream>
 #include"implementação.h"
 
@@ -6,8 +9,27 @@ using namespace std;
 
 
 
-#ifndef TESTES_H
-#define TESTES_H
+
+
+int main() {
+
+	int vetor[2] = {2,0},int inicio,fim;
+
+	quick_sort(vetor, 0, 2);
+	
+
+
+	for (int i = 0; i <= 6; i++) {
+		cout << vetor[i]<<"\t";
+	}
+
+}
+
+
+
+
+
+
 
 
 #endif
