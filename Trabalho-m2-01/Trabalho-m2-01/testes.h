e38e65c3e3443f3c2453d9e26e0955880e634dc6
Anderson Silva <anderson.silva@edu.univali.br>
2017-10-12 21:12:30 -0300

    organizando como mostrar os vetores ordenados

--- Trabalho-m2-01/Trabalho-m2-01/testes.h
+++ Trabalho-m2-01/Trabalho-m2-01/testes.h
@@ -7,21 +7,41 @@
 
 using namespace std;
 
+void mostra_vetor(int vetor[],int n) {
+	
 
+	for (int i = 0; i <= n; i++) {
+		cout << "|" << vetor[i] << "|";
+	}
+	cout << endl;
+
+
+}
 
 
 
 void teste() {
 
-	int vetor[] = {0,3,2,6,1},int inicio,fim;
+	int vetor_insertion1[] = { 0,3,2,6,1 };
+	float vetor_insertion2[] = { 1.3,2.5,5.6 , 10.3,9.6 };
+	char vetor_insertion3[] = { 'z','b','u','w','p' };
+	int inicio,fim;
 
-	insertion_sort(vetor, 5);
+
+
+	cout << "Este saos os vetores  ordenados com insertion: inteiro, real e caracteres" << endl;
+	insertion_sort(vetor_insertion1, 5);
+	cout << endl;
+	mostra_vetor(vetor_insertion1, 5);
+	cout << endl;
+	//insertion_sort(vetor_insertion2, 5);
+	//mostra_vetor(vetor_insertion2, 5);
+	cout << endl;
+	//insertion_sort(vetor_insertion3, 5);
+	//mostra_vetor(vetor_insertion3, 5);
+	cout << endl;
+
 	
-
-
-	for (int i = 0; i <= 5; i++) {
-		cout << vetor[i]<<"\t";
-	}
 
 }
 
