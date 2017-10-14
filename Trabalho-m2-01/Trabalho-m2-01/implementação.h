e02c994c5007a1e2e74fc1349c4539f1dc89261a
Anderson Silva <anderson.silva@edu.univali.br>
2017-10-14 14:35:05 -0300

    quick_sort  implementado

--- Trabalho-m2-01/Trabalho-m2-01/implementação.h
+++ Trabalho-m2-01/Trabalho-m2-01/implementação.h
@@ -19,12 +19,60 @@ void insertion_sort(int vetor[], int n) {
 	}
 }
 
+// funcoes do quick sort |
 
 
+void alternar(int vetor[], int i, int j) {
+	int auxiliar = vetor[i];
+	vetor[i] = vetor[j];
+	vetor[j] = auxiliar;
+}
+int dividir(int vetor[], int  comeco, int  fim) {//dividi o vetor ate nao ser mais possivel
+	int i = comeco, j = fim;
 
 
+	while (i < j) {
 
+		while ((i < fim) && (vetor[i] <= vetor[comeco])) {
+			i++;
+		}
+		while ((j>comeco) && (vetor[j] >= vetor[comeco])) {
+
+			j--;
+
+		}
+		if (i < j) {
+			alternar(vetor, i, j);
+			i++;
+			j--;
+		}
+
+	}
+	alternar(vetor, comeco, j);
+	return j;
+}
+
+
+void quick_sort(int vetor[], int comeco, int fim) {
+	if (comeco < fim) {
+
+		int comparador = dividir(vetor, comeco, fim);
+
+		quick_sort(vetor, comeco, comparador - 1);
+		quick_sort(vetor, comparador + 1, fim);
+
+	}
+
+}
 
 
 
 #endif
+
+
+
+
+
+
+
+
