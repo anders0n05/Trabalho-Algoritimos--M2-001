4f132d2087dad2f471f644a96b1b51d341721357
anders0n05 <30934037+anders0n05@users.noreply.github.com>
2017-10-17 13:37:25 -0200

    Alteracoes

--- Trabalho-m2-01/Trabalho-m2-01/testes.h
+++ Trabalho-m2-01/Trabalho-m2-01/testes.h
@@ -3,23 +3,17 @@
 #define TESTES_H
 
 #include<iostream>
-#include"implementação.h"
-
+#include"implementaÃ§Ã£o.h"
+#include<time.h>
+#include<stdlib.h>
 using namespace std;
+template<typename E>
+
+
 template<typename B>
-
-void mostra_vetor_original(B vetor[], int n) {
-
-	for (int i = 0; i < n; i++) {
-		cout << "|" << vetor[i] << "|";
-	}
-	cout << endl;
-
-}
-template<typename B>
-void mostra_vetor_ordenado(B vetor[],int n) {
+void mostra_vetor(B vetor[],int n) {
 	
-
+      
 	for (int i = 0; i < n; i++) {
 		cout << "|" << vetor[i] << "|";
 	}
@@ -32,56 +26,59 @@ void mostra_vetor_ordenado(B vetor[],int n) {
 
 void teste() {
 
-	int vetor_insertion1[] = { 0,3,2,6,1 };
-	float vetor_insertion2[] = { 1.30,2.50,5.60 , 10.30,0.2 };
-	char vetor_insertion3[] = { 'e','d','c','b','a' };
+	int vetor_int[50000] = {0};
+	float vetor_float[10] = {0};
+	char vetor_char[100] = {" "};
 	int inicio=0,fim=4,n=5;
 
-	int vetor_quick1[] = { 0,10,6,7,1 };
-	float vetor_quick2[] = { 2.1,2.5,2.6, 0.5,9.6 };
-	char vetor_quick3[] = { 'o','p','e','x','p' };
-
-	cout << " estes sao os vetores a ser ordenados por insertion_sort" << endl;
-
-	mostra_vetor_original(vetor_insertion1, n);
+	
+	cout << " estes sao os vetores a ser ordenados " << endl;
+	srand (time(NULL));
+	int elemento_int=rand()%100;
+	for (int i = 0; i < 50000; i++) {
+		
+		vetor_int[i]=elemento_int;
+		cout<<"|"<<vetor_int[i]<<"|";
+	}
 	cout << endl;
-	mostra_vetor_original(vetor_insertion2, n);
-	cout << endl;
-	mostra_vetor_original(vetor_insertion3,n);
-	cout << endl;
-
+         float elemento_float=rand()%10.0;
+	for (int i = 0; i < 10; i++) {
+		
+		vetor_float[i]=elemento_float;
+		cout<<"|"<<vetor_float[i]<<"|";
+	}
+	char elemento_char=rand()%26;
+	cout<<"endl;
+		for (int i = 0; i < 100; i++) {
+		
+		vetor_char[i]=elemento_char;
+		cout<<"|"<<vetor_char[i]<<"|";
+	}
+	
 	cout << "Este saos os vetores  ordenados com insertion: inteiro, real e caracteres" << endl;
-    insertion_sort<int,int>(vetor_insertion1, n);
+        insertion_sort<int,int>(vetor_int, 50000);
 	cout << endl;
-	mostra_vetor_ordenado<int>(vetor_insertion1, n);
+	mostra_vetor_ordenado<int>(vetor_int, 50000);
 	cout << endl;
-	insertion_sort<float,int>(vetor_insertion2,n);
-    mostra_vetor_ordenado<float>(vetor_insertion2,n);
+	insertion_sort<float,int>(vetor_float,10);
+        mostra_vetor_ordenado<float>(vetor_float,10);
 	cout << endl;
-	insertion_sort<char,int>(vetor_insertion3, n);
-	mostra_vetor_ordenado<char>(vetor_insertion3, n);
+	insertion_sort<char,int>(vetor_char,100);
+	mostra_vetor_ordenado<char>(vetor_char,100);
 	cout << endl;
 	
-	cout << " estes sao os vetores a ser ordenados por quick_sort" << endl;
-
-	cout << endl;
-	mostra_vetor_original(vetor_quick1, n);
-	cout << endl;
-	mostra_vetor_original<float>(vetor_quick2,n);
-	cout << endl;
-	mostra_vetor_original <char> (vetor_quick3, n);
-	cout << endl;
+	
 
 
 	cout << "Este saos os vetores  ordenados com quick sort: inteiro, real e caracteres" << endl;
-	quick_sort(vetor_quick1, inicio, fim);
-	mostra_vetor_ordenado<int>(vetor_quick1, n);
+	quick_sort(vetor_int, 0, 50000);
+	mostra_vetor_ordenado<int>(vetor_int, 50000);
 	cout << endl;
-	quick_sort(vetor_quick2,inicio,fim);
-	mostra_vetor_ordenado<float>(vetor_quick2, n);
+	quick_sort(vetor_float,0,10);
+	mostra_vetor_ordenado<float>(vetor_float, 10);
 	cout << endl;
-	quick_sort(vetor_quick3, inicio, fim);
-	mostra_vetor_ordenado<char>(vetor_quick3, n);
+	quick_sort(vetor_char, 0, 100);
+	mostra_vetor_ordenado<char>(vetor_char3, 100);
 	system("pause");
 }
 
